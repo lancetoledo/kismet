3dea886653b3164857dceefbc7a965878a607e71
Lance Toledo <lanceatoledo@gmail.com>
2022-04-29 19:14:35 -0400

    ADD: Flights Component

--- components/Banner.js
+++ components/Banner.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import FindFlights from './FindFlights'
 
 const styles = {
     banner: `flex flex-col w-full h-96 bg-light-green rounded-2xl p-[50px] gap-[30px]`,
     textWrapper:`w-6/12`,
     headline: `text-6xl text-white font-semibold pr-11`,
     slogin: `text-3xl text-white`,
-    select: `bg-neutral-900 text-white w-52 h-12 flex justify-center items-center rounded-t-xl font-semibold`
+    select: `bg-neutral-900 text-white w-52 h-12 flex justify-center items-center rounded-t-xl font-semibold`,
+    flightsWrapper: `w-4/5`,
+    flightsInfo: `h-20 w-full bg-neutral-50 rounded-b-xl rounded-tr-xl`
 }
 const Banner = () => {
   return (
     <div className={styles.banner}>
         <div className={styles.textWrapper}>
             <div className={styles.headline}>Explore New Places</div>
-            <div className={styles.slogin}>Enjoy every good moment</div>
+            <div className={styles.slogin}>Go where you are meant to be</div>
         </div>
 
         <div className={styles.flightsWrapper}>
-            <div className={styles.select}>Online Booking</div>
-            <div className={styles.flightsInfo}></div>
+            <div className={styles.select}>Flights</div>
+            <div className={styles.flightsInfo}>
+                <FindFlights/>
+            </div>
         </div>
     </div>
   )
---
+++ components/FindFlights.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FindFlights = () => {
+  return (
+    <div>FindFlights</div>
+  )
+}
+
+export default FindFlights
